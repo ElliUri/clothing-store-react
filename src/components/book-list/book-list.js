12f28b22948932aad5783acccb9969c683cda8c6
Eldana <fineshine6718@gmail.com>
2023-08-10 17:58:13 +0600

    changes book list file

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -52,3 +52,83 @@ export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
   )(BookList)
+
+// import React, { Component } from "react";
+// import BookListItem from "../book-list-item/book-list-item";
+// import { connect } from "react-redux";
+// import withBookstoreService from "../hoc/with-bookstore-service";
+// import { fetchBooks} from '../../actions/index'
+// import ErrorIndicator from '../eroor-indicator/error-indicator'
+// import s from "./book-list.module.css";
+// import compose from "../../utils";
+// import Spinner from '../spinner/spinner'
+
+
+// // class BookList extends Component {
+
+// //   render() {
+// //     const { books } = this.props;
+// //     return (
+// //       <ul>
+// //       {
+// //       books.map((book) => {
+// //         return (
+// //           <li key={book.id} className={s.book_list}>
+// //             <BookListItem book={book} />
+// //           </li>
+// //         );
+// //       })}
+// //     </ul>
+// //     )
+// //   }
+// // }
+
+// const BookList = ({books}) => {
+//   return (
+//     <ul>
+//       {
+//       books.map((book) => {
+//         return (
+//           <li key={book.id} 
+//           className={s.book_list}>
+//             <BookListItem book={book} />
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// }
+
+// class BookListContainer extends Component {
+  
+//   componentDidMount() {
+//     this.props.fetchBooks();
+//   }
+//   render() {
+//     const { books, loading, error } = this.props;
+
+//     if (loading) {
+//       return <Spinner /> 
+//     }
+
+//     if (error) {
+//       return <ErrorIndicator/>
+//     }
+//     return <BookList book={books} />
+//   }
+// }
+
+// const mapStateToProps = ({ books, loading, error }) => {
+//   return { books, loading, error };
+// };
+
+// const mapDispatchToProps = (dispatch,{bookstoreService})=> {
+//   return {
+//     fetchBooks: fetchBooks(bookstoreService, dispatch)
+//   }
+// };
+
+// export default compose(
+//   withBookstoreService(),
+//   connect(mapStateToProps, mapDispatchToProps)
+//   )(BookListContainer)
